890b910acd567425efb060842496bf81f3dc21bf
Nishidha Malviya <work.nishidha@gmail.com>
2026-06-06 03:13:07 +0530

    notUpdate page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
-
+#NEWCHANGENOTUPDATEDYET
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
